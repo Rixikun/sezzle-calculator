d26d3b92162aa635ca86eee478a2382ac185dc70
LinXia <linyianxia@hotmail.com>
2020-11-03 13:48:18 -0500

    add: logs style, more calculator style

--- src/components/HistoryLogs.js
+++ src/components/HistoryLogs.js
@@ -5,19 +5,20 @@ const HistoryLogs =(props)=> {
     const res = logs?.length
     ? logs.map((log, idx) => {
         const { calc } = log;
+        const left = calc.split("=")[0]
+        const right = calc.split("=")[1]
         return (
-          <div key={idx}>
-            <p>{calc}</p>
+          <div className="entry" key={idx}>
+            <p>{left} = <strong>{right}</strong></p>
           </div>
         );
       })
     : "";
     return (
-        <>
-        <div>
+        <div className="logs">
+            <h4>Past calculations: </h4>
           {res}  
         </div>
-        </>
     )
 }
 
