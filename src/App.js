8e31cd8de4abf340d6a92c9b0ca2b6b552eba68c
LinXia <linyianxia@hotmail.com>
2020-11-03 10:51:29 -0500

    require dotenv, add component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import DisplayHistory from './components/DisplayHistory'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>
+        Enter Calculations
+        </h1>
+      <br/>
+      <DisplayHistory />
     </div>
   );
 }
