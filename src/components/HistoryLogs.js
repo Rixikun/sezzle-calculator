446e2143274c9dd18148b0fbeb61a59f6e833fc7
LinXia <linyianxia@hotmail.com>
2020-11-03 10:55:09 -0500

    create HistoryLogs (returns data as html element)

---
+++ src/components/HistoryLogs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HistoryLogs =(props)=> {
+    const logs = props.logs
+    const res = logs?.length
+    ? logs.map((log, idx) => {
+        const { calc } = log;
+        return (
+          <div key={idx}>
+            <p>{calc}</p>
+          </div>
+        );
+      })
+    : "";
+    return (
+        <>
+        <div>
+
+          {res}  
+        </div>
+        </>
+    )
+}
+
+export default HistoryLogs
